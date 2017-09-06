6336752080ecf9c09b63b3cbe685af6038bfd204
Rievo <diegovm22@gmail.com>
2017-09-06 19:48:27 +0200

    Drawing the board

---
+++ board.js
@@ -0,0 +1,28 @@
+
+
+function Board(rows, cols){
+	this.rows = rows;
+	this.cols = cols;
+}
+
+
+Board.prototype.display = function(){
+
+	var rowDiv = height / this.rows;
+	var colsDiv = width / this.cols;
+
+
+	//Paint the background
+	fill(19,63,19);
+	rect(0,0, width, height);
+
+
+	fill(0,0,0,0);
+	stroke(0);
+	//Display each one of the tiles
+	for(var r = 0; r < this.rows; r++){
+		for(var c = 0; c < this.cols; c++){
+			rect(c * colsDiv, r * rowDiv, colsDiv, rowDiv);
+		}
+	}
+}
--- sketch.js
+++ sketch.js
@@ -1,12 +1,26 @@
 
+var board;
+
+
 function setup() {
-	createCanvas(400,400);
+	createCanvas(700,400);
 
 	
-
+	init();
 }
 
+
+
+//This function will be called each frame
 function draw() {
 	background(51);
 
+	board.display();
+}
+
+
+
+
+function init(){	
+	board = new Board(5,7);
 }
